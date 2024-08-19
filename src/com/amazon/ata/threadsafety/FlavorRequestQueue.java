fa16a7e128bcb393fde5c48859fd46086398d368
Alexander Rodriguez-Zingg <alexrodriguezzingg@icloud.com>
2024-08-19 13:27:48 -0600

    Completed guided video

--- src/com/amazon/ata/threadsafety/FlavorRequestQueue.java
+++ src/com/amazon/ata/threadsafety/FlavorRequestQueue.java
@@ -12,16 +12,16 @@ public class FlavorRequestQueue {
         flavorQueue = new LinkedList<>();
     }
 
-    public void needFlavor(Flavor flavor) {
+    public synchronized void needFlavor(Flavor flavor) {
         flavorQueue.add(flavor);
     }
 
     public Flavor nextNeededFlavor() {
-        Flavor flavor = flavorQueue.poll();
+        Flavor flavor = pollFlavorQueue();
         while (flavor == null) {
             try {
                 Thread.sleep(10L);
-                flavor = flavorQueue.poll();
+                flavor = pollFlavorQueue();
             } catch (InterruptedException e) {
                 System.out.println("!!!Interrupted waiting for flavor request!!!");
                 e.printStackTrace();
@@ -29,6 +29,10 @@ public class FlavorRequestQueue {
             }
         }
         return flavor;
+    }
+
+    private synchronized Flavor pollFlavorQueue() {
+        return flavorQueue.poll();
     }
 
     public int requestCount() {
